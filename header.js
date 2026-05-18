2bbe4f9258dfe109111120cb72de8e38e7729aad
harrypottertcgr <harrypottertcgr@gmail.com>
2026-05-18 18:49:51 -0400

    Add dynamic header HTML to header.js

--- header.js
+++ header.js
@@ -1,1 +1,7 @@
-<!js>
+const headerHTML = `
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+  </nav>
+`;
+document.getElementById('header-placeholder').innerHTML = headerHTML;
